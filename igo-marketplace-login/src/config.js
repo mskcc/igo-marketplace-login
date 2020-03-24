e26b8d15aa84ad1112169f33eefe3575809034c6
David Streid <david.streid.nyc@gmail.com>
2020-03-24 13:11:35 -0500

    Adding login

---
+++ igo-marketplace-login/src/config.js
@@ -0,0 +1,5 @@
+// DEV
+export const LOGIN_ENDPOINT = 'http://localhost:4000/api/auth/';
+export const TEST_ENDPOINT = 'http://localhost:4000/api/book/';
+// QA/PROD
+// export const LOGIN_ENDPOINT = '';
---
+++ igo-marketplace-login/src/services/login.js
@@ -0,0 +1,36 @@
+import {LOGIN_ENDPOINT, TEST_ENDPOINT} from "../config";
+import axios from 'axios';
+
+const getData = (resp) => {
+    const content = resp.data || {};
+    const data = content.data || {};
+    return data;
+};
+
+const HTTP = axios.create({
+    withCredentials: true
+})
+
+export function testGetEndpoint() {
+    /*
+    return new Promise((resolve) => { resolve(API_PROJECT) })
+        .then(resp => {return getData(resp)})
+        .catch(error => {throw new Error('Unable to get Get Events: ' + error) });
+     */
+    return axios
+        .get(`${TEST_ENDPOINT}`, { withCredentials: true })
+        .then(resp => {return getData(resp) })
+        .catch(error => {throw new Error('Unable to get Get Events: ' + error) });
+}
+
+export function sendLoginRequest(userName, password) {
+    /*
+    return new Promise((resolve) => { resolve(API_PROJECT) })
+        .then(resp => {return getData(resp)})
+        .catch(error => {throw new Error('Unable to get Get Events: ' + error) });
+     */
+    return axios
+        .post(`${LOGIN_ENDPOINT}/login`, { userName, password }, {withCredentials: true})
+        .then(resp => {return getData(resp) })
+        .catch(error => {throw new Error('Unable to get Get Events: ' + error) });
+}
