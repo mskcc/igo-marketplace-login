f0516e7ad44f5279ce89cb4acfb2f3072cc6b427
David Streid <david.streid.nyc@gmail.com>
2020-03-27 13:47:37 -0500

    Default Port: 4020

--- igo-marketplace-login/src/config.js
+++ igo-marketplace-login/src/config.js
@@ -1,5 +1,5 @@
 // DEV
-export const LOGIN_ENDPOINT = 'http://localhost:4000/api/auth';
-export const BOOK_ENDPOINT = 'http://localhost:4000/api/book';
+export const LOGIN_ENDPOINT = 'http://localhost:4020/api/auth';
+export const BOOK_ENDPOINT = 'http://localhost:4020/api/book';
 // QA/PROD
 // export const LOGIN_ENDPOINT = '';
