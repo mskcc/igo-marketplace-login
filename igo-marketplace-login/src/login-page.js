ed7ef719a4801ec0d13193132c53fa7d99295ee5
David Streid <david.streid.nyc@gmail.com>
2020-03-27 19:01:02 -0500

    Styling the login page

--- igo-marketplace-login/src/login-page.js
+++ igo-marketplace-login/src/login-page.js
@@ -3,25 +3,40 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 import Button from "@material-ui/core/Button";
-import {sendLoginRequest, getBooks} from "./services/login";
+import {sendLoginRequest} from "./services/login";
 
 const useStyles = makeStyles(theme => ({
     root: {
+        '& .MuiButtonBase-root': {
+            margin: 'auto',
+            display: 'block'
+        },
         '& .MuiTextField-root': {
             margin: theme.spacing(1),
             width: '25ch',
         },
-    },
+        'margin-top': '50px',
+        width: '275px',
+        margin: 'auto',
+        padding: '20px 15px',
+        'background-color': '#fff',
+        'border-radius': '6px',
+        'box-shadow': '0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1)',
+        color: '#4a4a4a',
+        display: 'block',
+    }
 }));
 
 function LoginPage({match}) {
     // Take redirect prop, which should be a url that the login page will reroute to
     const classes = useStyles();
 
-    const redirect = match.params.redirect;
+    // Redirect-url is the path that will be routed to after a successful login
+    const redirectPath = match['url'] || '/';
 
-    const [username, setUserName] = useState('streidd');
-    const [password, setPassword] = useState('Lucky3gg');
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [prompt, setPrompt] = useState('Please enter your login details below');
 
     const changeUserName = (evt) => {
         const userName = evt.target.value;
@@ -37,45 +52,41 @@ function LoginPage({match}) {
         return username.length === 0 || password.length === 0;
     };
 
-    const testRedirect = () => {
-        console.log(redirect);
-        window.location.href = `http://${redirect}`;
-    }
-
     const login = () => {
-        sendLoginRequest(username, password, redirect).then(() => {
-            // window.location.href = redirect;
+        setPrompt('Logging in...');
+        sendLoginRequest(username, password).then(() => {
+            setPrompt('Login Successful');
+            window.location.href = redirectPath;
+        }).catch((err) => {
+            const resp = err.response || {};
+            const data = resp.data || {};
+            const msg = data.message || 'Login Failed';
+            setPrompt(msg);
         })
-    };
-
-    const books = () => {
-        getBooks();
     };
 
     return (<form className={classes.root} noValidate autoComplete="off">
                 <div>
-                    <TextField required
-                               id="standard-required"
-                               label="Required"
-                               value={username} onChange={changeUserName}
-                    />
-                    <TextField required
-                               id="standard-password-input"
-                               label="Password"
-                               type="password"
-                               autoComplete="current-password"
-                               value={password} onChange={changePassword}/>
+                    <div className={"block"}>
+                        <p className={"text-align-center"}>{prompt}</p>
+                    </div>
+                    <div className={"block"}>
+                        <TextField required
+                                   id="standard-required"
+                                   label="Required"
+                                   value={username} onChange={changeUserName}/>
+                    </div>
+                    <div className={"block"}>
+                        <TextField required
+                                   id="standard-password-input"
+                                   label="Password"
+                                   type="password"
+                                   autoComplete="current-password"
+                                   value={password} onChange={changePassword}/>
+                    </div>
                     <Button variant="contained" disabled={validInputs()}
                             onClick={login}>
                         Submit
-                    </Button>
-                    <Button variant="contained"
-                            onClick={testRedirect}>
-                        Redirect
-                    </Button>
-                    <Button variant="contained"
-                            onClick={books}>
-                        Books
                     </Button>
                 </div>
             </form>
--- igo-marketplace-login/src/services/login.js
+++ igo-marketplace-login/src/services/login.js
@@ -1,9 +1,5 @@
-import {BOOK_ENDPOINT, LOGIN_ENDPOINT} from "../config";
+import {LOGIN_ENDPOINT} from "../config";
 import axios from 'axios';
 export function sendLoginRequest(userName, password, redirect) {
     return axios.post(`${LOGIN_ENDPOINT}/login`, { userName, password, redirect }, {withCredentials: true});
-}
-
-export function getBooks() {
-    return axios.get(`${BOOK_ENDPOINT}/`, {withCredentials: true});
 }
