a2482304ae21c57739932b6ae77e8f3cba91d0e2
David Streid <david.streid.nyc@gmail.com>
2020-06-19 11:25:15 -0400

    Running linter & removing Book endpoints

--- igo-marketplace-login-backend/models/UserModel.js
+++ igo-marketplace-login-backend/models/UserModel.js
@@ -1,24 +1,24 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-	firstName: {type: String, required: true},
-	lastName: {type: String, required: true},
-	username: {type: String, required: true},
-	title: {type: String, required: true},
-	groups: {type: String, required: true},
-	isLabMember: {type: Boolean, required: true},
-	isAdmin: {type: Boolean, required: true},
-	isPM: {type: Boolean, required: true},
-	isUser: {type: Boolean, required: true},
-	loginFirstDate: {type: Date, required: true},
-	loginLastDate: {type: Date, required: true},
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  username: {type: String, required: true},
+  title: {type: String, required: true},
+  groups: {type: String, required: true},
+  isLabMember: {type: Boolean, required: true},
+  isAdmin: {type: Boolean, required: true},
+  isPM: {type: Boolean, required: true},
+  isUser: {type: Boolean, required: true},
+  loginFirstDate: {type: Date, required: true},
+  loginLastDate: {type: Date, required: true},
 }, {timestamps: true});
 
 // Virtual for user's full name
 UserSchema
-	.virtual("fullName")
-	.get(function () {
-		return this.firstName + " " + this.lastName;
-	});
+  .virtual('fullName')
+  .get(function () {
+    return this.firstName + ' ' + this.lastName;
+  });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
