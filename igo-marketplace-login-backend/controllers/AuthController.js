f80f2d8cdde7161dc3c4592f72811658e9df2891
Stephanie DelBelso <sdelbelso@grubhub.com>
2024-03-01 12:35:01 -0500

    testing

--- igo-marketplace-login-backend/controllers/AuthController.js
+++ igo-marketplace-login-backend/controllers/AuthController.js
@@ -213,6 +213,8 @@ exports.login = [
 				// Successful login - prepare valid JWT token for future authentication
 				cookieValidator.setJwtToken(res, jwtPayload);
 
+				console.log(res);
+				// console.log(jwtPayload);
 				// Log cookie size to verify nginx buffer_size will not be exceeded
 				const jwtPayloadString = JSON.stringify(jwtPayload);
 				logger.log("info", `JWT Token Set: ${Buffer.byteLength(jwtPayloadString, 'utf8')} bytes. Sending successful login response for User: ${user}`);
